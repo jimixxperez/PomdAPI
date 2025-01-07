5c8152b6ac13f5bb091e4ac6f96fbb38786e593a
Jeremy Perez <jeremy.perez@pwc.com>
2025-01-07 09:29:12 +0100

    added caching strategy

--- cache/in_memory.py
+++ cache/in_memory.py
@@ -3,18 +3,22 @@ from typing import Generic, Iterable, Optional
 from core.types import (
     SyncCachingStrategy,
     AyncCachingStrategy,
-    EndpointDefinitionGen,
     TResponse,
     Tag,
 )
+from core.api import EndpointDefinitionGen
+from core.caching import Cache
 
 
-class InMemoryKVStore(Generic[TResponse]):
+class InMemoryBackend(Generic[TResponse]):
     def __init__(self):
         self._store: dict[str, TResponse] = {}
 
     def delete(self, key: str) -> None:
         del self._store[key]
+
+    async def adelete(self, key: str) -> None:
+        return self.delete(key)
 
     def get(self, key: str) -> Optional[TResponse]:
         return self._store.get(key)
@@ -22,41 +26,13 @@ class InMemoryKVStore(Generic[TResponse]):
     async def aget(self, key: str) -> Optional[TResponse]:
         return self.get(key)
 
-    def set(self, key: str, value: TResponse) -> None:
+    def set(self, key: str, value: TResponse, ttl: Optional[int] = None) -> None:
         self._store[key] = value
 
-    def aset(self, key: str, value: TResponse) -> None:
+    async def aset(self, key: str, value: TResponse, ttl: Optional[int] = None) -> None:
         self.set(key, value)
 
 
-@dataclass
-class InMemoryCache(
-    Generic[EndpointDefinitionGen, TResponse],
-):
-    _cache: dict[str, TResponse]
-
-    def get(
-        self,
-        endpoint_name: str,
-        request: EndpointDefinitionGen,
-        tags: Iterable[str | Tag],
-    ) -> Optional[TResponse]: ...
-
-    def aget(
-        self,
-        endpoint_name: str,
-        request: EndpointDefinitionGen,
-        tags: Iterable[str | Tag],
-    ): ...
-
-    async def aset(
-        self,
-        endpoint_name: str,
-        request: EndpointDefinitionGen,
-        response: TResponse,
-        tags: Iterable[str | Tag],
-    ) -> None: ...
-
-    def invalidate_tags(self, tags: Iterable[str | Tag]) -> None: ...
-
-    async def ainvalidate_tags(self, tags: Iterable[str | Tag]) -> None: ...
+class InMemoryCache(Cache[EndpointDefinitionGen, TResponse]):
+    def __init__(self):
+        super().__init__(_backend=InMemoryBackend[TResponse]())
--- core/caching.py
+++ core/caching.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass
 from typing import Generic, Iterable, Protocol, Optional
 
@@ -8,26 +9,46 @@ from core.types import TResponse, Tag
 class CacheBackend(Protocol[TResponse]):
     def delete(self, key: str) -> None: ...
 
+    async def adelete(self, key: str) -> None: ...
+
     def get(self, key: str) -> Optional[TResponse]: ...
 
     async def aget(self, key: str) -> Optional[TResponse]: ...
 
     def set(self, key: str, value: TResponse, ttl: Optional[int] = None) -> None: ...
 
-    def aset(self, key: str, value: TResponse, ttl: Optional[int] = None) -> None: ...
+    async def aset(
+        self, key: str, value: TResponse, ttl: Optional[int] = None
+    ) -> None: ...
 
 
 @dataclass
 class Cache(Generic[EndpointDefinitionGen, TResponse]):
     _backend: CacheBackend[TResponse]
 
+    @staticmethod
+    def key_from_req(endpoint_name: str, request: EndpointDefinitionGen) -> str:
+        return f"{endpoint_name}/{request}"
+
+    @staticmethod
+    def key_from_tag(endpoint_name: str, tag: str | Tag) -> str:
+        return f"{endpoint_name}/tag/{tag}"
+
     def get_by_request(
         self,
         endpoint_name: str,
         request: EndpointDefinitionGen,
     ) -> Optional[TResponse]:
-        key = f"{endpoint_name}/{request}"
+        key = self.key_from_req(endpoint_name, request)
         return self._backend.get(key)
+
+    async def aget_by_request(
+        self,
+        endpoint_name: str,
+        request: EndpointDefinitionGen,
+    ) -> Optional[TResponse]:
+        key = self.key_from_req(endpoint_name, request)
+        return await self._backend.aget(key)
 
     def get_by_tags(
         self,
@@ -35,6 +56,45 @@ class Cache(Generic[EndpointDefinitionGen, TResponse]):
         tags: Iterable[str | Tag],
     ) -> Optional[TResponse]:
         for tag in tags:
-            key = f"{endpoint_name}/tags/{tag}"
+            key = self.key_from_tag(endpoint_name, tag)
             if response := self._backend.get(key):
                 return response
+
+    async def aget_by_tags(
+        self,
+        endpoint_name: str,
+        tags: Iterable[str | Tag],
+    ) -> Optional[TResponse]:
+        for tag in tags:
+            key = self.key_from_tag(endpoint_name, tag)
+            if response := await self._backend.aget(key):
+                return response
+
+    async def aset(
+        self,
+        endpoint_name: str,
+        request: EndpointDefinitionGen,
+        tags: Iterable[str | Tag],
+        response: TResponse,
+        ttl: Optional[int] = None,
+    ) -> None:
+        key = self.key_from_req(endpoint_name, request)
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self._backend.aset(key, response, ttl=ttl))
+            for tag in tags:
+                key = self.key_from_tag(endpoint_name, tag)
+                tg.create_task(self._backend.aset(key, response, ttl=ttl))
+
+    def invalidate_tags(self, endpoint_name: str, tags: Iterable[str | Tag]) -> None:
+        for tag in tags:
+            key = self.key_from_tag(endpoint_name, tag)
+            self._backend.delete(key)
+
+    async def ainvalidate_tags(
+        self, endpoint_name: str, tags: Iterable[str | Tag]
+    ) -> None:
+        async with asyncio.TaskGroup() as tg:
+            for tag in tags:
+                key = self.key_from_tag(endpoint_name, tag)
+                tg.create_task(self._backend.adelete(key))
+
